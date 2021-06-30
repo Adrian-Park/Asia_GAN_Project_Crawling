1bd1c31d175f20b621b39b35c250d85342ed46d2
Adrian-Park <korhammers@gmail.com>
2021-06-30 19:38:54 +0900

    2019년 네이버 영화 크롤링

--- prj2_Movie_for_you/crawling/naver_review_crawling.py
+++ prj2_Movie_for_you/crawling/naver_review_crawling.py
@@ -3,44 +3,47 @@
 #crawling은 각자 진행하고 빨리 완성되는 코드로 연도를 나눠서 진행
 #우선 2019년 개봉작 크롤링, 나머지는 연도별로 크롤링
 #데이터는 데이터프레임으로 작업 후 저장 형식은 csv
-#컬렴 명은 ['year','titles','reviews']로 통일
+#컬렴 명은 ['titles','reviews']로 통일
 #파일명은 'reviews_0000.csv'로 만들기 / 0000은 연도
 #크롤링 파일은 https://url.kr/rjbvyg 에 업로드
 
-#이런 식으로 사전에 통일감 있게 만들기
-class crawling_naver() :
-    def crawling (self, page, url) :
-        pass
-
-    def data_save (self, path) :
-        pass
-
+# 모듈 임포트!
 from selenium import webdriver
 import pandas as pd
-from selenium.common.exceptions import NoSuchElementException #오류가 있을 때 지나간다
+from selenium.common.exceptions import NoSuchElementException
 import time
 
+# 구글 드라이브 옵션 설정
 options = webdriver.ChromeOptions()
-#options.add_argument('headless') #크롬 열리는게 안 보임
+# options.add_argument('headless')
 options.add_argument('disable_gpu')
 options.add_argument('lang=ko_KR')
 
+# 드라이버 불러오기 및 설정
 driver = webdriver.Chrome('chromedriver', options=options)
-years = []
+
+# 영화제목과 리뷰를 담을 빈 리스트 생성
 titles = []
 reviews = []
 
-# https://movie.naver.com/movie/sdb/browsing/bmovie.nhn?open=2019&page=1
-#//*[@id="old_content"]/ul/li[1]/a
-#//*[@id="old_content"]/ul/li[10]/a
+# 작동하는지 테스트
+# url = 'https://movie.naver.com/movie/sdb/browsing/bmovie.nhn?open=2019&page=43'
+# driver.get(url)
+# y = driver.find_elements_by_xpath('//*[@id="old_content"]/ul/li')
+# print(len(y))
+#
+# driver.find_element_by_xpath(f'//*[@id="old_content"]/ul/li[1]/a').click()
+# # driver.find_element_by_xpath(f'//*[@id="old_content"]/ul/li[%d]/a'%j) # 변수 지정 다른 방법
+# time.sleep(0.5) # 페이지 로드를 위한 시간
+# driver.find_element_by_xpath('//*[@id="movieEndTabMenu"]/li[6]/a/em').click()
 
-
-#2019년 개봉영화 리스트
+# 페이지와 제목 크롤링
 try:
-    for i in range(1, 44): # 2019년 개봉영화 리스트 44
+    for i in range(1, 44): # 연도별 영화 리스트 페이지수 확인
         url = f'https://movie.naver.com/movie/sdb/browsing/bmovie.nhn?open=2019&page={i}'
-        time.sleep(0.5)
-        for j in range(1, 21): # 영화 갯수
+
+
+        for j in range(1, 21): # 영화 제목 한 페이지당 20개
             try: # 제목
                 driver.get(url)
                 time.sleep(1)
@@ -76,7 +79,7 @@ try:
                                         time.sleep(1)
                                     except:
                                         driver.back()
-                                        time.sleep(0.5)
+                                        time.sleep(1)
                                         print('review crawling error')
                                 except:
                                     time.sleep(1)
@@ -90,13 +93,12 @@ try:
                 time.sleep(1)
                 print('NoSuchElementException')
 
-        print(i,'번째 페이지 완료')
-
-        df_review = pd.DataFrame({'titles':titles, 'reviews':reviews})
+        print(len(titles))
+        df_review = pd.DataFrame({'titles': titles, 'reviews': reviews})
         df_review['years'] = 2019
         print(df_review.head(20))
-        df_review.to_csv('./reviews_2019_{}_page.csv')
-        print(i, '번째 페이지 저장 완료')
+        df_review.to_csv(f'./reviews_2019_{i}_page.csv')
+
 except:
     print('except1')
 finally:
