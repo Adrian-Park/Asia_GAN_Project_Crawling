443ea45c5517141a9a37321e9602df47efc0a44f
Adrian-Park <korhammers@gmail.com>
2021-07-01 17:40:12 +0900

    영화 당 리뷰 1개로 병합

--- prj2_Movie_for_you/preprocess_one_sentence.py
+++ prj2_Movie_for_you/preprocess_one_sentence.py
@@ -1,8 +1,8 @@
 import pandas as pd
 
-df = pd.read_csv('./crawling/cleaned_review_2019.csv', index_col=0)
+df = pd.read_csv('./crawling/cleaned_review_2020.csv', index_col=0)
 df.dropna(inplace=True)
-df.to_csv('./crawling/cleaned_review_2019.csv')
+df.to_csv('./crawling/cleaned_review_2020.csv')
 
 one_sentences = [] # 빈 리스트 생성
 for title in df['titles'].unique(): # 영화 제목들 한 번씩만
@@ -10,7 +10,12 @@ for title in df['titles'].unique(): # 영화 제목들 한 번씩만
     one_sentence = ' '.join(temp) # 리뷰들 하나로 이어붙이기
     one_sentences.append(one_sentence)
 
-df_one_sentences = pd.DataFrame({'title':df['titles'].unique(), 'reviews':one_sentences})
+df_one_sentences = pd.DataFrame({'titles':df['titles'].unique(), 'reviews':one_sentences})
 print(df_one_sentences.head())
 
-df_one_sentences.to_csv('./crawling/one_sentence_review_2019.csv')
+df_one_sentences.to_csv('./crawling/one_sentence_review_2020.csv')
+
+# for idx, title in enumerate(df['titles'].unique()):
+#     temp = df[df['titles'] == title]['cleaned_reviews']
+#     one_senetece = ' '.join(temp)
+#     one_sentence
