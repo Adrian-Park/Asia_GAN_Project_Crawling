3492279d44798038831c1eec0624d00195cad5cc
Adrian-Park <korhammers@gmail.com>
2021-07-01 17:40:19 +0900

    워드클라우드

--- prj2_Movie_for_you/wordcloud_exam.py
+++ prj2_Movie_for_you/wordcloud_exam.py
@@ -15,19 +15,19 @@ font_name = font_manager.FontProperties(fname=fontpath).get_name()
 rc('font', family=font_name)
 mpl.font_manager._rebuild()
 
-df = pd.read_csv('./crawling/cleaned_review_2019.csv', index_col=0)
+df = pd.read_csv('./crawling/one_sentence_review_2019.csv', index_col=0)
 df.dropna(inplace=True) # nan값 제거
 # print(df.info())
 #
 # print(df.head())
 
 # 지정 인덱스 값의 제목이 같은 로우 찾기
-movie_index = df[df['titles'] == '고질라: 킹 오브 몬스터 (Godzilla: King of the Monsters)'].index[32]
+movie_index = df[df['titles'] == '0.0MHz'].index[0]
 print(movie_index)
-print(df.cleaned_sentences[movie_index])
+print(df.reviews[movie_index])
 
 # 단어 리스트에 띄어쓰기 기준으로 단어 잘라내어 담기
-words = df.cleaned_sentences[movie_index].split(' ')
+words = df.reviews[movie_index].split(' ')
 print(words)
 
 # 단어사전 생성
@@ -41,7 +41,7 @@ stopwords = ['관객', '작품']
 # stopword 적용 O
 wordcloud_img = WordCloud(background_color='white', max_words=2000,
                           font_path=fontpath,
-                          stopwords=stopwords).generate(df.cleaned_sentences[movie_index])
+                          stopwords=stopwords).generate(df.reviews[movie_index])
 # stopword 적용 X
 wordcloud_img = WordCloud(background_color='white', max_words=2000,
                           font_path=fontpath,).generate_from_frequencies(worddict)
@@ -49,5 +49,5 @@ wordcloud_img = WordCloud(background_color='white', max_words=2000,
 plt.figure(figsize=(8,8))
 plt.imshow(wordcloud_img, interpolation='bilinear')
 plt.axis('off') # 눈금 X
-plt.title(df.titles[movie_index], size=25)
+plt.title(df.reviews[movie_index], size=25)
 plt.show()
