a66491aea1050e0a3e95fb01457008a5aac6775e
Adrian-Park <korhammers@gmail.com>
2021-07-02 16:03:29 +0900

    word2vec

--- prj2_Movie_for_you/word2vec.py
+++ prj2_Movie_for_you/word2vec.py
@@ -18,14 +18,14 @@ for sentence in cleaned_token_review:
 
 # Word2Vec 파라미터 설명 https://hoonzi-text.tistory.com/2
 embedding_moel = Word2Vec(cleaned_tokens, vector_size=100, window=4, min_count=20, workers=4, epochs=100, sg=1)
-# 축소할 차원 크기 (단어 하나 당 좌표 100개)
-# 커널사이즈 (훈련 시 앞 뒤로 고려하는 단어 개수)
-# 최소 빈도 수. 해당 빈도수보다 적게 등장하면 학습서 배제
-# CPU 사용 갯수
-# 학습 횟수
-# 어떠한 알고리즘을 사용할 지 (sg는 Skip-gram)
+# vector_size 축소할 차원 크기 (단어 하나 당 좌표 100개)
+# window 커널사이즈 (훈련 시 앞 뒤로 고려하는 단어 개수)
+# min_count 최소 빈도 수. 해당 빈도수보다 적게 등장하면 학습서 배제
+# workers CPU 사용 갯수
+# epochs 학습 횟수
+# sg 어떠한 알고리즘을 사용할 지 (sg는 Skip-gram)
 
 embedding_moel.save('./models/word2VecModel_2016_2021.model')
 
-print(embedding_moel.wv.vocab.keys())
-print(len(embedding_moel.wv.vocab.keys()))
+# print(embedding_model.wv.vocab.keys())
+# print(len(embedding_model.wv.vocab.keys()))
